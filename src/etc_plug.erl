491a10770e7ebf5d32fdcb4686bea53d37af9e55
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2020-11-29 18:38:37 +0100

    getting all files from rebar

--- src/etc_plug.erl
+++ src/etc_plug.erl
@@ -1,9 +1,9 @@
 -module(etc_plug).
 
-% -export([init/1]).
+-export([init/1]).
 
-% -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-% init(State) ->
-%     io:format("Args: ~p~n", ["Hello etc_plug"]),
-%     {ok, State1} = etc_plug_prv:init(State),
-%     {ok, State1}.
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    io:format("Args: ~p~n", ["Hello etc_plug"]),
+    {ok, State1} = etc_plug_prv:init(State),
+    {ok, State1}.
--- src/etc_plug_prv.erl
+++ src/etc_plug_prv.erl
@@ -4,6 +4,10 @@
 
 -define(PROVIDER, etc_plug).
 -define(DEPS, [app_discovery]).
+%% PRINT Debugging macro%%
+-ifndef(PRINT).
+-define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
+-endif.
 
 %% ===================================================================
 %% Public API
@@ -26,10 +30,82 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    io:format("Args: ~p~n", ["do prv"]),
-    {ok, State}.
+    io:format("Do state~n"),
+    % {ok, State}.
+    CodePaths = rebar_state:code_paths(State, all_deps),
+    ?PRINT(CodePaths),
+    code:add_pathsa(rebar_state:code_paths(State, all_deps)),
+    CheckedApps = lists:map(fun gradualizer_check_app/1, rebar_state:project_apps(State)),
+    HasNok = lists:member(nok, CheckedApps),
+    if
+        HasNok -> {error, {?MODULE, undefined}};
+        true -> {ok, State}
+    end.
 
--spec format_error(any()) ->  iolist().
-format_error(Reason) ->
-    io:format("Args: ~p~n", ["format error prv"]),
-    io_lib:format("~p", [Reason]).
+% -spec format_error(any()) ->  iolist().
+% format_error(Reason) ->
+%     io:format("Args: ~p~n", ["format error prv"]),
+%     io_lib:format("~p", [Reason]).
+
+-spec format_error(any()) -> string().
+format_error(_) ->
+    "ETC found errors.".
+
+-spec gradualizer_check_app(rebar_app_info:t()) -> ok | nok.
+gradualizer_check_app(App) ->
+    GOpts = rebar_app_info:get(App, gradualizer_opts, []),
+    Files = files_to_check(App),
+    ?PRINT(GOpts),
+    ?PRINT(Files),
+    ok.
+
+-spec files_to_check(rebar_app_info:t()) -> [file:name()].
+files_to_check(App) ->
+    Opts = rebar_app_info:opts(App),
+    GOpts = rebar_app_info:get(App, gradualizer_opts, []),
+    Include = proplists:get_value(include, GOpts, undefined),
+    Exclude = proplists:get_value(exclude, GOpts, []),
+    Cwd = rebar_app_info:dir(App),
+
+    Patterns = case Include of
+        undefined ->
+            {SrcDirs, ExtraDirs} = resolve_src_dirs(Opts),
+            lists:map(fun(File) ->
+                filename:join(filename:absname(File, Cwd), "*.erl")
+            end, SrcDirs ++ ExtraDirs);
+        _ -> Include
+    end,
+    Files = lists:flatmap(fun (Pattern) ->
+            filelib:wildcard(filename:absname(Pattern, Cwd))
+        end, Patterns),
+    ExpandedFiles = lists:flatmap(fun (Dir) ->
+            case filelib:is_dir(Dir) of
+                true ->
+                    filelib:wildcard(filename:join(Dir, "*.{erl,beam}"));
+                false ->
+                    [Dir]
+            end
+        end, Files),
+    ExpandedExclude = lists:flatmap(fun (Pattern) ->
+                filelib:wildcard(filename:absname(Pattern, Cwd))
+            end, Exclude),
+    lists:filter(
+        fun (File) ->
+            not lists:member(File, ExpandedExclude)
+        end, ExpandedFiles).
+
+
+
+-spec resolve_src_dirs(dict:dict()) -> {[file:name()], [file:name()]}.
+resolve_src_dirs(Opts) ->
+    SrcDirs = rebar_dir:src_dirs(Opts, ["src"]),
+    ExtraDirs = rebar_dir:extra_src_dirs(Opts, []),
+    normalize_src_dirs(SrcDirs, ExtraDirs).
+
+%% remove duplicates and make sure no directories that exist
+%% in src_dirs also exist in extra_src_dirs
+-spec normalize_src_dirs([file:name()], [file:name()]) -> {[file:name()], [file:name()]}.
+normalize_src_dirs(SrcDirs, ExtraDirs) ->
+    S = lists:usort(SrcDirs),
+    E = lists:subtract(lists:usort(ExtraDirs), S),
+    {S, E}.
